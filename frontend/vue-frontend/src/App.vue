33410a47443e9e0d9ed560dd0a17fd79502ce485
Muhi03 <akinci.muhammed@hotmail.com>
2021-12-29 18:13:38 +0100

    added frontend files

---
+++ frontend/vue-frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Customer />
+  </div>
+</template>
+
+<script>
+import Customer from './components/Customer.vue'
+export default {
+  name: 'App',
+  components: {
+    Customer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/vue-frontend/src/components/Customer.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <h1 class="text-center"> Customer list</h1>
+        <table class="table table-striped"> 
+         <thead> 
+             <th> Customer ID </th>
+             <th> Customer Firstname </th>
+             <th> Customer Lastname </th>
+             <th> Customer Company </th>
+             <th> Customer E-Mail </th>
+         </thead>
+         <tbody> 
+             <tr v-for= "customer in customers" v-bind:key = "customer.id"> 
+                 <td> {{ customer.id }} </td>
+                 <td> {{ customer.firstName }} </td>
+                 <td> {{ customer.lastName }}</td>
+                 <td> {{ customer.company }}</td>
+                 <td> {{ customer.email }} </td>
+             </tr>
+         </tbody>
+        </table>
+         </div>
+</template>
+
+<script>
+ import CustomerService from '../services/CustomerService'
+  export default {
+      name:'Customers',
+      data() {
+          return {
+              customers : []
+          }
+
+      },
+      methods: {
+          getCustomers() {
+              CustomerService.getCustomers().then((response) =>{
+                  this.customers = response.data;
+              });
+          }
+      },
+      created() {
+          this.getCustomers()
+      }
+    }
+</script>
+
