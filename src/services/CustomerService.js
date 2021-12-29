ae517ed916a404a3e5c821c1074bb45388f80226
Lala Mammadova <a01652485@unet.univie.ac.at>
2021-12-29 17:27:16 +0100

    frontend pushed

---
+++ src/services/CustomerService.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const CUSTOMER_API_BASE_URL = 'http://localhost:8080/api/customers'
+
+
+class CustomerService {
+    getCustomers(){
+        return axios.get(CUSTOMER_API_BASE_URL);
+    }
+}
+
+export default new CustomerService()
